efb98f964cb25833c25670995f4e39ce09fe3aa2
simonhuchede <simon.huchede@7opteam.com>
2023-01-31 00:19:23 +0100

    testDoubles

---
+++ OHCE.Test.xUnit/TestDoubles/LangueMock.cs
@@ -0,0 +1,18 @@
+﻿using OHCESimonH;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE.Test.xUnit.TestDoubles
+{
+    internal record LangueMock : ILangue
+    {
+        public string BienDit { get; init; } = string.Empty;
+
+        public string Bonjour { get; init; } = string.Empty;
+
+        public string AuRevoir { get; init; } = string.Empty;
+    }
+}
---
+++ OHCE.Test.xUnit/TestDoubles/LangueStub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE.Test.xUnit.TestDoubles
+{
+    internal record LangueStub : LangueMock
+    {
+    }
+}
